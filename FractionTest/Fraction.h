b0f22c82ae8d313507fdd9c5c4bbfa4d75ca42cd
Sander Peerna <sanderpeerna@gmail.com>
2015-04-12 13:36:53 -0700

    Add synthesized accessor methods.

--- FractionTest/Fraction.h
+++ FractionTest/Fraction.h
@@ -12,11 +12,9 @@
 
 @interface Fraction : NSObject
 
+@property int numerator, denominator;
+
 -(void)   print;
--(void)   setNumerator: (int) n;
--(void)   setDenominator: (int) d;
--(int)    numerator;
--(int)    denominator;
 -(double) convertToNum;
 
 @end
--- FractionTest/main.m
+++ FractionTest/main.m
@@ -12,11 +12,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Fraction *myFraction = [[Fraction alloc] init];
         
-        [myFraction setNumerator: 1];
-        [myFraction setDenominator: 3];
+        myFraction.numerator = 1;
+        myFraction.denominator = 3;
         
-        NSLog(@"The value of my fraction is:");
-        [myFraction print];
+        NSLog(@"The numerator is %i, and the denominator is %i.", myFraction.numerator, myFraction.denominator);
     }
     
     return 0;
